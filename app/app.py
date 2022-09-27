f230f8869bcbbdb30b2f52c0681bf0cc36322a91
Theme Frugal <arunram.atmacharan@iiml.org>
2022-09-27 23:40:42 +0530

    Incremental Changes

--- app/app.py
+++ app/app.py
@@ -3,3 +3,14 @@
 
 import pandas as pd
 import streamlit as st
+
+# Here is the top level outline
+# Multi page
+# 1. Nav Charting
+# 2. CAGR Analysis
+# 3. Rolling Return Analysis
+# 4. SIP Analysis
+# 5. STP Analysis
+# 6. SWP Analysis
+
+st.plotly_chart(fig_1)
