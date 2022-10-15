1962ae92131a3ba8955b0fa06c95140d366ac031
Theme Frugal <arunram.atmacharan@iiml.org>
2022-10-15 21:31:47 +0530

    MF insights - initial file

---
+++ app/insights.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+# To generate insights based MF history - NAV, CAGR, Comparison,
+# Scenario analysis
+# Actual analysis
+# AUM analysis along with categorization
